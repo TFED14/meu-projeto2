ae40db5ca222b5be42c0053214ba22419e6c46c8
TFED14 <168959693+TFED14@users.noreply.github.com>
2024-06-20 15:18:36 -0300

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -4,9 +4,10 @@ function store(){
   var lowerCaseLetters = /[a-z]/g;
   var upperCaseLetters = /[A-Z]/g;
   var numbers = /[0-9]/g;
+  var currentDate = new Date();
 
   if(Name.value.length == 0){
-    alert('Informe um email');
+    alert('Informe um username');
   }else if(!Name.value.match(upperCaseLetters)){
     alert('Deve conter uma letra maiuscula');
   }else if(!Name.value.match(lowerCaseLetters)){
@@ -14,8 +15,17 @@ function store(){
   }else{
     localStorage.setItem('Name', Name.value);
     localStorage.setItem('Email', Email.value);
+    localStorage.setItem('RegistrationDate', currentDate);
     alert('Sua conta foi criada');
   }
+
+  var li = document.createElement("li");
+  var textNode = document.createTextNode("Username: " + Name.value + ", Email:" + Email.value + ", Data de Inscrição: " + currentDate.toLocaleString());
+  li.appendChild(textNode);
+  document.getElementById("cadastroLista").appendChild(li);
+
+  document.getElementById('Name').value = "";
+  document.getElementById('Email').value = "";
 }
 
 function check(){
